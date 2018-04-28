579d8e1864e7f99411c0e42bc271dff037a7a1f9
KotoriChiu <zxcv88928@gmail.com>
2018-04-29 00:51:14 +0800

    準完工

--- 判斷程式是否運作中.c
+++ 判斷程式是否運作中.c
@@ -3,7 +3,7 @@
 int main(){
 	while(1){
 		Sleep(1000);
-			if(FindWindow(0,"Train Simulator")){ //用法 將要偵測的程式視窗名字 貼上
+			if(FindWindow(0,"Train Simulator")){ //用法 將要偵測的程式視窗名字貼上
 		printf("YES\n");
       }else{
       	printf("NO\n");
