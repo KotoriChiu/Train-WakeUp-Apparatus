9e04f61b043d79dba6b3f7058846a4a5325c141b
KotoriChiu <zxcv88928@gmail.com>
2018-04-29 12:03:46 +0800

    修改無法在未啟動警醒裝置時 計時 導致啟用時時間錯誤的BUG

--- keyboard.c
+++ keyboard.c
@@ -7,29 +7,44 @@
 */
 int main(){
     int train = 1;
+    int aaaa = 1;
     while(1){
+    	printf("等待遊戲啟動\n");
+    	Sleep(1000);
     while(FindWindow(0,"Train Simulator") && !access("D:/Steam/steamapps/common/RailWorks/Write Cache",0)){ //Write Cache遊戲執行時產生的資料夾 檢查用 可以不用加
-        char a ;
+        if(aaaa){
+        	printf("遊戲文件已偵測到 開始執行!!!\n");
+        	aaaa = 0;
+		}
+		char a ;
         char *filename = "G:/yee/teraterm.log"; //log文件位子 請自行修改
         FILE *fp = fopen(filename,"r");
         char buf[1000];
         int lineCnt = 0;
         while(fgets(buf,1000,fp)) lineCnt++;
         fclose(fp);
-        printf(buf);
         Sleep(100);//延遲個100毫秒 防止程式崩潰
-        if(buf == 's' && train == 1){ //停車指令條件成立
-            train = 0;
+        printf(buf);
+        printf("\n");
+        if(buf[0] == 's' && train == 1){ //停車指令條件成立 
+            printf("停車!!!!\n");
             //虛擬鍵盤用法 
             //keybd_event(鍵碼,0,0,0)//這是按下
             //keybd_event(鍵碼,0,KEYEVENTF_KEYUP,0)//這是放掉
               
              //請注意 如果你的遊戲中的緊急煞車制動不是BackSpace鍵 那麼你得自己改下面的按鍵數據
+             //這邊的按鍵動作是先啟動列車的緊急停車制動 再將電門放掉 然後再將電韌開到最大
             keybd_event(8, MapVirtualKey(8, 0), 0, 0);
             Sleep(300);
 		    keybd_event(8, MapVirtualKey(8, 0), KEYEVENTF_KEYUP, 0);
-        }else if(train != 1 && buf != 's') train = 1; 
-        
+		    keybd_event(68, MapVirtualKey(68, 0), 0, 0);
+            Sleep(2000);
+            keybd_event(68, MapVirtualKey(68, 0), KEYEVENTF_KEYUP, 0);
+            keybd_event(222, MapVirtualKey(222, 0), 0, 0);
+		    Sleep(2000);
+            keybd_event(222, MapVirtualKey(222, 0), KEYEVENTF_KEYUP, 0);
+            train = 0;
+        }else if(buf[0] != 's')train = 1;     
         }
     }
  return 0;
--- 警醒裝置.ino
+++ 警醒裝置.ino
@@ -5,7 +5,7 @@ int step_up_reciprocal = 90;        //放開時倒數秒數
 unsigned long previousTime = 0;     //儲存系統運作時間(故用unsigned long)
 int uno1 = 2;                       //傳給音效uno板的訊號線1
 int uno2 = 3;                       //傳給音效uno板的訊號線2
-
+int bibi = 1;
 
 void setup(){
     pinMode(sw,INPUT);      //警醒裝置開關設為讀取
@@ -15,10 +15,30 @@ void setup(){
     Serial.begin(9600);     //設定鮑率9600
 }
 void loop(){
+       unsigned long step_off = millis()/1000;  //在未啟用警醒裝置狀態下 因為倒數是依據millis在計數的 所以在這裡也要把previousTime給更新 否則會出錯
+        if(step_off - previousTime > 1){
+            previousTime++;
+        }
     if(digitalRead(powersw) == HIGH){   //決定要不要啟用警醒裝置
-    while(digitalRead(sw)==HIGH){      
-        if(digitalRead(powersw) == LOW) break;
-        step_up_reciprocal = 10;
+    if(bibi == 1){
+        digitalWrite(2,LOW);
+        digitalWrite(3,HIGH);
+        bibi = 0;
+        delay(100);
+        digitalWrite(2,LOW); 
+        digitalWrite(3,LOW);
+    }
+    while(digitalRead(sw)==HIGH){     
+      Serial.print("現在時間");
+      Serial.println(step_on_reciprocal);
+        if(digitalRead(powersw) == LOW){
+          Serial.println("Yee");
+            bibi = 1;
+            digitalWrite(2,LOW);
+            digitalWrite(3,LOW);
+            break;
+        }
+        step_up_reciprocal = 90;
         Serial.println("a");
         unsigned long step_on = millis()/1000;
         if(step_on - previousTime > 1){
@@ -47,9 +67,17 @@ void loop(){
             digitalWrite(3,LOW);
         }
     }
-    while(digitalRead(sw)==LOW){    
-        if(digitalRead(powersw) == LOW) break;
-        step_on_reciprocal = 20;
+    while(digitalRead(sw)==LOW){  
+        Serial.print("現在時間");
+        Serial.println(step_up_reciprocal);  
+        if(digitalRead(powersw) == LOW){
+            bibi = 1;
+            digitalWrite(2,LOW);
+            digitalWrite(3,LOW);
+            Serial.println("Yee");
+            break;
+        }
+        step_on_reciprocal = 90;
         Serial.println("a");
         unsigned long step_up = millis()/1000;
         if(step_up - previousTime > 1){
